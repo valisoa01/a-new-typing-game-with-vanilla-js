2a72a368392230ac5346b3559272c31f4802cc13
valisoa01 <valisoatolotriniaina@gmail.com>
2026-01-28 19:28:10 +0300

    feat: add redirectory

---
+++ public/assets/script/bulle.js
@@ -0,0 +1,85 @@
+function createBubbles() {
+    const container = document.querySelector('.bubbles-container');
+    const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const bubbleCount = 30;
+
+    container.innerHTML = '';
+    
+    for (let i = 0; i < bubbleCount; i++) {
+        const bubble = document.createElement('div');
+        bubble.className = 'bubble';
+        const randomLetter = letters[Math.floor(Math.random() * letters.length)];
+        bubble.textContent = randomLetter;
+        const size = Math.random() * 40 + 20;
+        bubble.style.width = `${size}px`;
+        bubble.style.height = `${size}px`;
+        bubble.style.fontSize = `${size * 0.6}px`
+        bubble.style.left = `${Math.random() * 100}%`;
+        const duration = Math.random() * 20 + 10;
+        bubble.style.animationDuration = `${duration}s`;
+        bubble.style.animationDelay = `${Math.random() * 5}s`;
+        
+        container.appendChild(bubble);
+    }
+}
+                                                                                                                                                                                                               document.addEventListener('DOMContentLoaded', createBubbles);
+checkbox.addEventListener('change', () => {
+    setTimeout(createBubbles, 300); 
+})
+window.addEventListener('resize', createBubbles);
+
+document.addEventListener('DOMContentLoaded', function() {
+    const userImage = document.getElementById('userImage');
+    const chevron = document.querySelector('.fa-chevron-right');
+    const userDropdown = document.getElementById('userDropdown');
+    const dropdownBackdrop = document.createElement('div');
+    dropdownBackdrop.className = 'dropdown-backdrop';
+    document.body.appendChild(dropdownBackdrop);
+  
+    function toggleProfileMenu(e) {
+      e.stopPropagation();
+      
+      const isShowing = userDropdown.classList.toggle('show');
+      
+
+      userImage.classList.toggle('active', isShowing);
+      chevron.classList.toggle('active', isShowing);
+      dropdownBackdrop.classList.toggle('active', isShowing);
+      
+
+      if (isShowing) {
+        document.addEventListener('click', closeProfileMenu);
+        window.addEventListener('scroll', closeProfileMenu);
+      } else {
+        removeEventListeners();
+      }
+    }
+  
+    function closeProfileMenu(e) {
+      if (!userDropdown.contains(e.target) && !userImage.contains(e.target)) {
+        userDropdown.classList.remove('show');
+        userImage.classList.remove('active');
+        chevron.classList.remove('active');
+        dropdownBackdrop.classList.remove('active');
+        removeEventListeners();
+      }
+    }
+  
+    function removeEventListeners() {
+      document.removeEventListener('click', closeProfileMenu);
+      window.removeEventListener('scroll', closeProfileMenu);
+    }
+  
+    userImage.addEventListener('click', toggleProfileMenu);
+    
+
+    document.addEventListener('keydown', function(e) {
+      if (e.key === 'Escape' && userDropdown.classList.contains('show')) {
+        userDropdown.classList.remove('show');
+        userImage.classList.remove('active');
+        chevron.classList.remove('active');
+        dropdownBackdrop.classList.remove('active');
+        removeEventListeners();
+      }
+    });
+  });
---
+++ public/assets/script/signIn.js
@@ -0,0 +1,34 @@
+function handleCredentialResponse(response) {
+   const data = parseJwt(response.credential);
+  console.log("Connecté avec :", data);
+  
+   localStorage.setItem("user", JSON.stringify(data));
+
+   window.location.href = "/assets/pages/menu.html";
+}
+
+function parseJwt(token) {
+  const base64Url = token.split('.')[1];
+  const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+  const jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
+    return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+  }).join(''));
+  return JSON.parse(jsonPayload);
+}
+
+window.onload = () => {
+  google.accounts.id.initialize({
+    client_id: "940374287458-jblc2ahrdplmjctghabpmgirdiauuvjc.apps.googleusercontent.com",
+    callback: handleCredentialResponse
+  });
+  google.accounts.id.renderButton(
+    document.getElementById("g_id_signin"),
+    { theme: "outline", size: "large" }
+  );
+};
+
+ 
+    
+  
+
+ 
